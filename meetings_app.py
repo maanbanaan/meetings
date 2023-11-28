582edcc935d8bb25acfee2e91775067789327ec8
maanbanaan <134083894+maanbanaan@users.noreply.github.com>
2023-11-28 13:37:01 +0100

    Update meetings_app.py

--- meetings_app.py
+++ meetings_app.py
@@ -1,16 +1,18 @@
 import streamlit as st
 
 if 'button' not in st.session_state:
-    st.session_state.button = False
+    st.session_state.button = 0
 
 def click_button():
-    st.session_state.button = not st.session_state.button
+    st.session_state.button = (st.session_state.button + 1) % 3
 
 st.button('Click me', on_click=click_button)
 
-if st.session_state.button:
+if st.session_state.button == 0:
     # The message and nested widget will remain on the page
-    st.write('Button is on!')
+    st.write('Meeting not started.')
     st.slider('Select a value')
+elif st.session_state.button == 1:
+    st.write('Meeting in progress!')
 else:
-    st.write('Button is off!')
+    st.write('Meeting finished.')
