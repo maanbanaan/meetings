36fc9104625b37c719a6f3a439f3d9acee49b4c6
maanbanaan <134083894+maanbanaan@users.noreply.github.com>
2023-11-28 13:52:07 +0100

    Update meetings_app.py

--- meetings_app.py
+++ meetings_app.py
@@ -8,7 +8,7 @@ for i in range(1, NUM_TEAMS + 1):
 
 def click_button(team_number):
     st.session_state[team_number] = (st.session_state[team_number] + 1) % 3
-col1, col2 = st.columns([1,1,1])
+col1, col2 = st.columns([1,1])
 for i in range(1, NUM_TEAMS + 1):
     with col1:
         st.button(f'Status team {i}', on_click=click_button, args=[i])
