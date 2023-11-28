7849a697e2866a51c7e70540f411a20acc585ac9
maanbanaan <134083894+maanbanaan@users.noreply.github.com>
2023-11-28 13:51:14 +0100

    Update meetings_app.py

--- meetings_app.py
+++ meetings_app.py
@@ -8,13 +8,15 @@ for i in range(1, NUM_TEAMS + 1):
 
 def click_button(team_number):
     st.session_state[team_number] = (st.session_state[team_number] + 1) % 3
-
+col1, col2 = st.columns([1,1,1])
 for i in range(1, NUM_TEAMS + 1):
-    st.button(f'Status team {i}', on_click=click_button, args=[i])
-    if st.session_state[i] == 0:
-        # The message and nested widget will remain on the page
-        st.write('Meeting not started.')
-    elif st.session_state[i] == 1:
-        st.write('Meeting in progress!')
-    else:
-        st.write('Meeting finished.')
+    with col1:
+        st.button(f'Status team {i}', on_click=click_button, args=[i])
+    with col2:
+        if st.session_state[i] == 0:
+            # The message and nested widget will remain on the page
+            st.write('Meeting not started.')
+        elif st.session_state[i] == 1:
+            st.write('Meeting in progress!')
+        else:
+            st.write('Meeting finished.')
