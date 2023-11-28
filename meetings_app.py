8797943999955a20d28bb1930cf1857fd4d54cac
maanbanaan <134083894+maanbanaan@users.noreply.github.com>
2023-11-28 13:38:39 +0100

    Update meetings_app.py

--- meetings_app.py
+++ meetings_app.py
@@ -6,7 +6,8 @@ if 'button' not in st.session_state:
 def click_button():
     st.session_state.button = (st.session_state.button + 1) % 3
 
-st.button('Click me', on_click=click_button)
+for i in range(1, 14):
+    st.button(f'Status team {i}', on_click=click_button)
 
 if st.session_state.button == 0:
     # The message and nested widget will remain on the page
