65f6c00728b0479d4d27518c42d3b40b50d61de8
maanbanaan <134083894+maanbanaan@users.noreply.github.com>
2023-11-28 16:21:03 +0100

    Update meetings_app.py

--- meetings_app.py
+++ meetings_app.py
@@ -1,16 +1,31 @@
 import streamlit as st
+import requests
+import pandas as pd
+
+# Defining API to access spreadsheet
+API = "https://api.apispreadsheets.com/data/PEiZQxeLHxAruOzL/"
 
 NUM_TEAMS = 12
 
 rows = dict()
 
+# Getting initial status for each team
+r = requests.get("https://api.apispreadsheets.com/data/PEiZQxeLHxAruOzL/")
+initial_data = pd.DataFrame(r.json()['data'])
+
 for i in range(1, NUM_TEAMS + 1):
     if i not in st.session_state:
-        st.session_state[i] = 0
+        st.session_state[i] = df.loc[df['team'] == i, 'state'].values[0]
     rows[i] = st.columns([0.2, 0.8])
 
 def click_button(team_number):
-    st.session_state[team_number] = (st.session_state[team_number] + 1) % 3
+    new_state = (st.session_state[team_number] + 1) % 3
+    # Update session state
+    st.session_state[team_number] = new_state
+
+    # Update spreadsheet
+    r = requests.post("https://api.apispreadsheets.com/data/PEiZQxeLHxAruOzL/", headers={}, json={"data": {"state":f"{new_state}"}, "query": f"select * from PEiZQxeLHxAruOzL where team='{team_number}'"})
+    
 for i in range(1, NUM_TEAMS + 1):
     with rows[i][0]:
         st.button(f'Status team {i}', on_click=click_button, args=[i])
