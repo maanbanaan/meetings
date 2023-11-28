45960822c2d3f71a83fd37ce7f8c38b80178454c
maanbanaan <134083894+maanbanaan@users.noreply.github.com>
2023-11-28 13:21:32 +0100

    Update meetings_app.py
    Changed functionality; buttons have been added and selection boxes have been removed

--- meetings_app.py
+++ meetings_app.py
@@ -1,21 +1,44 @@
 import streamlit as st
 import pandas as pd
 
+# Create a DataFrame to store the meeting schedule
 schedule_data = pd.DataFrame({
     'Team Number': range(1, 11),
     'Meeting Status': ['Not Started'] * 10
 })
 
+def toggle_status(team_number):
+    current_status = schedule_data.loc[schedule_data['Team Number'] == team_number, 'Meeting Status'].values[0]
+    
+    if current_status == 'Not Started':
+        schedule_data.loc[schedule_data['Team Number'] == team_number, 'Meeting Status'] = 'In Progress'
+    elif current_status == 'In Progress':
+        schedule_data.loc[schedule_data['Team Number'] == team_number, 'Meeting Status'] = 'Complete'
+    elif current_status == 'Complete':
+        schedule_data.loc[schedule_data['Team Number'] == team_number, 'Meeting Status'] = 'Not Started'
+
 def main():
     st.title('Meeting Status App')
 
-    team_number = st.selectbox('Select Team Number:', range(1, 11))
-    meeting_status = st.radio('Meeting Status:', ['Not Started', 'In Progress', 'Completed'])
+    # Display the table with a button for each row
+    for index, row in schedule_data.iterrows():
+        team_number = row['Team Number']
+        meeting_status = row['Meeting Status']
+        
+        col1, col2, col3 = st.beta_columns([1, 1, 6])
 
-    # Update the schedule_data DataFrame based on user input
-    schedule_data.loc[schedule_data['Team Number'] == team_number, 'Meeting Status'] = meeting_status
+        with col1:
+            st.write(f"Team {team_number}")
 
+        with col2:
+            st.write(meeting_status)
+
+        with col3:
+            if st.button(f"Toggle Status ({team_number})", key=f"button_{team_number}"):
+                toggle_status(team_number)
+
+    # Display the updated table
     st.table(schedule_data)
-    
+
 if __name__ == '__main__':
     main()
