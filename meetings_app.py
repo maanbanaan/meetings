537713b71553b837399068fe211c47b2793e48ef
maanbanaan <134083894+maanbanaan@users.noreply.github.com>
2023-11-28 13:43:57 +0100

    Update meetings_app.py

--- meetings_app.py
+++ meetings_app.py
@@ -1,18 +1,20 @@
 import streamlit as st
 
-if 'button' not in st.session_state:
-    st.session_state.button = 0
+NUM_TEAMS = 12
 
-def click_button():
-    st.session_state.button = (st.session_state.button + 1) % 3
+for i in range(1, NUM_TEAMS + 1):
+    if i not in st.session_state:
+        st.session_state[i] = 0
+
+def click_button(team_number):
+    st.session_state.button = (st.session_state[team_number] + 1) % 3
 
 for i in range(1, 14):
-    st.button(f'Status team {i}', on_click=click_button)
-
-if st.session_state.button == 0:
-    # The message and nested widget will remain on the page
-    st.write('Meeting not started.')
-elif st.session_state.button == 1:
-    st.write('Meeting in progress!')
-else:
-    st.write('Meeting finished.')
+    st.button(f'Status team {i}', on_click=click_button, args=[i])
+    if st.session_state[i] == 0:
+        # The message and nested widget will remain on the page
+        st.write('Meeting not started.')
+    elif st.session_state[i] == 1:
+        st.write('Meeting in progress!')
+    else:
+        st.write('Meeting finished.')
