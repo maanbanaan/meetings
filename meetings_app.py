126796a7f28acdedad3d71f650bdfe43f21eb3d0
maanbanaan <134083894+maanbanaan@users.noreply.github.com>
2023-11-29 17:43:43 +0100

    Update meetings_app.py

--- meetings_app.py
+++ meetings_app.py
@@ -3,6 +3,27 @@ import requests
 import pandas as pd
 import gspread
 
+def load_data(credentials, sheet_name):
+    gc = gspread.service_account_from_dict(credentials)
+    sh = gc.open(sheet_name)
+    wks = sh.worksheet("Sheet1")
+    st.session_state.data = pd.DataFrame(wks.get_all_values())
+    # return pd.DataFrame(wks.get_all_values())
+
+def click_button(team_number):
+    new_state = (st.session_state[team_number] + 1) % 3
+    # Update session state
+    st.session_state[team_number] = new_state
+
+    # Update spreadsheet
+    wks.update(range_name = f'B{team_number + 1}', values = new_state)
+    
+    # r = requests.post("https://api.apispreadsheets.com/data/PEiZQxeLHxAruOzL/", headers={}, json={"data": {"state":f"{new_state}"}, "query": f"select * from PEiZQxeLHxAruOzL where team='{team_number}'"})
+
+def submit():
+    st.session_state.user_input = st.session_state.widget
+    st.session_state.widget = ''
+
 google_credentials = {cred: st.secrets["google_creds"][cred] for cred in st.secrets["google_creds"]}
 
 # Password to enter edit mode
@@ -31,27 +52,6 @@ for i in range(1, NUM_TEAMS + 1):
     if i not in st.session_state:
         st.session_state[i] = st.session_state.data.loc[st.session_state.data['team'] == i, 'state'].values[0]
     rows[i] = st.columns([0.2, 0.8])
-    
-def load_data(credentials: dict, sheet_name: str):
-    gc = gspread.service_account_from_dict(credentials)
-    sh = gc.open(sheet_name)
-    wks = sh.worksheet("Sheet1")
-    st.session_state.data = pd.DataFrame(wks.get_all_values())
-    # return pd.DataFrame(wks.get_all_values())
-
-def click_button(team_number):
-    new_state = (st.session_state[team_number] + 1) % 3
-    # Update session state
-    st.session_state[team_number] = new_state
-
-    # Update spreadsheet
-    wks.update(range_name = f'B{team_number + 1}', values = new_state)
-    
-    # r = requests.post("https://api.apispreadsheets.com/data/PEiZQxeLHxAruOzL/", headers={}, json={"data": {"state":f"{new_state}"}, "query": f"select * from PEiZQxeLHxAruOzL where team='{team_number}'"})
-
-def submit():
-    st.session_state.user_input = st.session_state.widget
-    st.session_state.widget = ''
 
 if 'user_input' not in st.session_state:
     st.session_state.user_input = ''
