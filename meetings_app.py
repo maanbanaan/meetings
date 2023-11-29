4e388dffa6aa07d186792918ea9b78cc6650c52b
maanbanaan <134083894+maanbanaan@users.noreply.github.com>
2023-11-29 12:42:16 +0100

    Update meetings_app.py

--- meetings_app.py
+++ meetings_app.py
@@ -5,6 +5,10 @@ import gspread
 for cred in st.secrets["google_creds"]:
     st.write(cred)
 
+credentials = {cred: st.secrets["google_creds"][cred] for cred in st.secrets["google_creds"]}
+st.write(credentials)
+st.write(st.secrets["google_creds"])
+
 schedule_data = pd.DataFrame({
     'Team Number': range(1, 11),
     'Meeting Status': ['Not Started'] * 10
