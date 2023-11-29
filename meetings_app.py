5891b6d6b434fe1f31d25ed781899aa2446df153
maanbanaan <134083894+maanbanaan@users.noreply.github.com>
2023-11-29 17:38:07 +0100

    Update meetings_app.py

--- meetings_app.py
+++ meetings_app.py
@@ -1,17 +1,22 @@
 import streamlit as st
 import requests
 import pandas as pd
+import gspread
 
-# Defining API to access spreadsheet
-API = "https://api.apispreadsheets.com/data/PEiZQxeLHxAruOzL/"
+google_credentials = {cred: st.secrets["google_creds"][cred] for cred in st.secrets["google_creds"]}
+
+# Password to enter edit mode
+pw = st.secrets["DB_PASS"]
+
+# # Defining API to access spreadsheet
+# API = "https://api.apispreadsheets.com/data/PEiZQxeLHxAruOzL/"
 
 if 'initialized' not in st.session_state:
     st.session_state.initialized = False
 
 if not st.session_state.initialized:
     # Getting initial status for each team
-    r = requests.get("https://api.apispreadsheets.com/data/PEiZQxeLHxAruOzL/")
-    st.session_state.data = pd.DataFrame(r.json()['data'])
+    load_data(google_credentials, st.secrets.sheet_name)
     st.session_state.initialized = True
 
 # Setting number of teams
@@ -20,15 +25,19 @@ NUM_TEAMS = len(st.session_state.data)
 # If False, the user can only view the team status, not change it
 st.session_state.EDIT = False
 
-# Required to enter edit mode
-pw = st.secrets["DB_PASS"]
-
 # Setting up formatting
 rows = dict()
 for i in range(1, NUM_TEAMS + 1):
     if i not in st.session_state:
         st.session_state[i] = st.session_state.data.loc[st.session_state.data['team'] == i, 'state'].values[0]
     rows[i] = st.columns([0.2, 0.8])
+    
+def load_data(credentials: dict, sheet_name: str):
+    gc = gspread.service_account_from_dict(credentials)
+    sh = gc.open(sheet_name)
+    wks = sh.worksheet("Sheet1")
+    st.session_state.data = pd.DataFrame(wks.get_all_values())
+    # return pd.DataFrame(wks.get_all_values())
 
 def click_button(team_number):
     new_state = (st.session_state[team_number] + 1) % 3
@@ -36,7 +45,9 @@ def click_button(team_number):
     st.session_state[team_number] = new_state
 
     # Update spreadsheet
-    r = requests.post("https://api.apispreadsheets.com/data/PEiZQxeLHxAruOzL/", headers={}, json={"data": {"state":f"{new_state}"}, "query": f"select * from PEiZQxeLHxAruOzL where team='{team_number}'"})
+    wks.update(range_name = f'B{team_number + 1}', values = new_state)
+    
+    # r = requests.post("https://api.apispreadsheets.com/data/PEiZQxeLHxAruOzL/", headers={}, json={"data": {"state":f"{new_state}"}, "query": f"select * from PEiZQxeLHxAruOzL where team='{team_number}'"})
 
 def submit():
     st.session_state.user_input = st.session_state.widget
